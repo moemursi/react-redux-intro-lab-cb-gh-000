aa2859b42ae48a0d4697be4eeee4d94aa6e50110
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-01-02 13:02:39 -0500

    Remove root node of 'game' from gameReducer state

--- test/game-reducer-test.js
+++ test/game-reducer-test.js
@@ -1,67 +1,5 @@
 import gameReducer from '../src/reducers/gameReducer'
 
-// const initialState = {
-//   game: {
-//     winner: null,
-//     userCards: [],
-//     aiCards: [],
-//     deck: [
-//      {name:"Ace of Diamonds", value: 1},
-//      {name:"Ace of Spades", value: 1},
-//      {name:"Ace of Clubs", value: 1},
-//      {name:"Ace of Hearts", value: 1},
-//      {name:"Two of Diamonds", value: 2},
-//      {name:"Two of Spades", value: 2},
-//      {name:"Two of Clubs", value: 2},
-//      {name:"Two of Hearts", value: 2},
-//      {name:"Three of Diamonds", value: 3},
-//      {name:"Three of Spades", value: 3},
-//      {name:"Three of Clubs", value: 3},
-//      {name:"Three of Hearts", value: 3},
-//      {name:"Four of Diamonds", value: 4},
-//      {name:"Four of Spades", value: 4},
-//      {name:"Four of Clubs", value: 4},
-//      {name:"Four of Hearts", value: 4},
-//      {name:"Five of Diamonds", value: 5},
-//      {name:"Five of Spades", value: 5},
-//      {name:"Five of Clubs", value: 5},
-//      {name:"Five of Hearts", value: 5},
-//      {name:"Six of Diamonds", value: 6},
-//      {name:"Six of Spades", value: 6},
-//      {name:"Six of Clubs", value: 6},
-//      {name:"Six of Hearts", value: 6},
-//      {name:"Seven of Diamonds", value: 7},
-//      {name:"Seven of Spades", value: 7},
-//      {name:"Seven of Clubs", value: 7},
-//      {name:"Seven of Hearts", value: 7},
-//      {name:"Eight of Diamonds", value: 8},
-//      {name:"Eight of Spades", value: 8},
-//      {name:"Eight of Clubs", value: 8},
-//      {name:"Eight of Hearts", value: 8},
-//      {name:"Nine of Diamonds", value: 9},
-//      {name:"Nine of Spades", value: 9},
-//      {name:"Nine of Clubs", value: 9},
-//      {name:"Nine of Hearts", value: 9},
-//      {name:"Ten of Diamonds", value: 10},
-//      {name:"Ten of Spades", value: 10},
-//      {name:"Ten of Clubs", value: 10},
-//      {name:"Ten of Hearts", value: 10},
-//      {name:"Jack of Diamonds", value: 10},
-//      {name:"Jack of Spades", value: 10},
-//      {name:"Jack of Clubs", value: 10},
-//      {name:"Jack of Hearts", value: 10},
-//      {name:"Queen of Diamonds", value: 10},
-//      {name:"Queen of Spades", value: 10},
-//      {name:"Queen of Clubs", value: 10},
-//      {name:"Queen of Hearts", value: 10},
-//      {name:"King of Diamonds", value: 10},
-//      {name:"King of Spades", value: 10},
-//      {name:"King of Clubs", value: 10},
-//      {name:"King of Hearts", value: 10}
-//    ]
-//   }
-// }
-
 function hasSameProps( obj1, obj2 ) {
   return Object.keys( obj1 ).every( function( prop ) {
     return obj2.hasOwnProperty( prop );
@@ -71,72 +9,70 @@ function hasSameProps( obj1, obj2 ) {
 describe('gameReducer', () => {
   beforeEach(function(){
     this.initialState = {
-      game: {
-        winner: null,
-        userCards: [],
-        aiCards: [],
-        deck: [
-         {name:"Ace of Diamonds", value: 1},
-         {name:"Ace of Spades", value: 1},
-         {name:"Ace of Clubs", value: 1},
-         {name:"Ace of Hearts", value: 1},
-         {name:"Two of Diamonds", value: 2},
-         {name:"Two of Spades", value: 2},
-         {name:"Two of Clubs", value: 2},
-         {name:"Two of Hearts", value: 2},
-         {name:"Three of Diamonds", value: 3},
-         {name:"Three of Spades", value: 3},
-         {name:"Three of Clubs", value: 3},
-         {name:"Three of Hearts", value: 3},
-         {name:"Four of Diamonds", value: 4},
-         {name:"Four of Spades", value: 4},
-         {name:"Four of Clubs", value: 4},
-         {name:"Four of Hearts", value: 4},
-         {name:"Five of Diamonds", value: 5},
-         {name:"Five of Spades", value: 5},
-         {name:"Five of Clubs", value: 5},
-         {name:"Five of Hearts", value: 5},
-         {name:"Six of Diamonds", value: 6},
-         {name:"Six of Spades", value: 6},
-         {name:"Six of Clubs", value: 6},
-         {name:"Six of Hearts", value: 6},
-         {name:"Seven of Diamonds", value: 7},
-         {name:"Seven of Spades", value: 7},
-         {name:"Seven of Clubs", value: 7},
-         {name:"Seven of Hearts", value: 7},
-         {name:"Eight of Diamonds", value: 8},
-         {name:"Eight of Spades", value: 8},
-         {name:"Eight of Clubs", value: 8},
-         {name:"Eight of Hearts", value: 8},
-         {name:"Nine of Diamonds", value: 9},
-         {name:"Nine of Spades", value: 9},
-         {name:"Nine of Clubs", value: 9},
-         {name:"Nine of Hearts", value: 9},
-         {name:"Ten of Diamonds", value: 10},
-         {name:"Ten of Spades", value: 10},
-         {name:"Ten of Clubs", value: 10},
-         {name:"Ten of Hearts", value: 10},
-         {name:"Jack of Diamonds", value: 10},
-         {name:"Jack of Spades", value: 10},
-         {name:"Jack of Clubs", value: 10},
-         {name:"Jack of Hearts", value: 10},
-         {name:"Queen of Diamonds", value: 10},
-         {name:"Queen of Spades", value: 10},
-         {name:"Queen of Clubs", value: 10},
-         {name:"Queen of Hearts", value: 10},
-         {name:"King of Diamonds", value: 10},
-         {name:"King of Spades", value: 10},
-         {name:"King of Clubs", value: 10},
-         {name:"King of Hearts", value: 10}
-       ]
-      }
+      winner: null,
+      userCards: [],
+      aiCards: [],
+      deck: [
+        {name:"Ace of Diamonds", value: 1},
+        {name:"Ace of Spades", value: 1},
+        {name:"Ace of Clubs", value: 1},
+        {name:"Ace of Hearts", value: 1},
+        {name:"Two of Diamonds", value: 2},
+        {name:"Two of Spades", value: 2},
+        {name:"Two of Clubs", value: 2},
+        {name:"Two of Hearts", value: 2},
+        {name:"Three of Diamonds", value: 3},
+        {name:"Three of Spades", value: 3},
+        {name:"Three of Clubs", value: 3},
+        {name:"Three of Hearts", value: 3},
+        {name:"Four of Diamonds", value: 4},
+        {name:"Four of Spades", value: 4},
+        {name:"Four of Clubs", value: 4},
+        {name:"Four of Hearts", value: 4},
+        {name:"Five of Diamonds", value: 5},
+        {name:"Five of Spades", value: 5},
+        {name:"Five of Clubs", value: 5},
+        {name:"Five of Hearts", value: 5},
+        {name:"Six of Diamonds", value: 6},
+        {name:"Six of Spades", value: 6},
+        {name:"Six of Clubs", value: 6},
+        {name:"Six of Hearts", value: 6},
+        {name:"Seven of Diamonds", value: 7},
+        {name:"Seven of Spades", value: 7},
+        {name:"Seven of Clubs", value: 7},
+        {name:"Seven of Hearts", value: 7},
+        {name:"Eight of Diamonds", value: 8},
+        {name:"Eight of Spades", value: 8},
+        {name:"Eight of Clubs", value: 8},
+        {name:"Eight of Hearts", value: 8},
+        {name:"Nine of Diamonds", value: 9},
+        {name:"Nine of Spades", value: 9},
+        {name:"Nine of Clubs", value: 9},
+        {name:"Nine of Hearts", value: 9},
+        {name:"Ten of Diamonds", value: 10},
+        {name:"Ten of Spades", value: 10},
+        {name:"Ten of Clubs", value: 10},
+        {name:"Ten of Hearts", value: 10},
+        {name:"Jack of Diamonds", value: 10},
+        {name:"Jack of Spades", value: 10},
+        {name:"Jack of Clubs", value: 10},
+        {name:"Jack of Hearts", value: 10},
+        {name:"Queen of Diamonds", value: 10},
+        {name:"Queen of Spades", value: 10},
+        {name:"Queen of Clubs", value: 10},
+        {name:"Queen of Hearts", value: 10},
+        {name:"King of Diamonds", value: 10},
+        {name:"King of Spades", value: 10},
+        {name:"King of Clubs", value: 10},
+        {name:"King of Hearts", value: 10}
+      ]
     }
   })
   
   describe('default case', function() {
     it('should return the initial state', function() {
       const newState = gameReducer(undefined, {})
-      expect(hasSameProps(newState, this.initialState.game)).toEqual(true)
+      expect(hasSameProps(newState, this.initialState)).toEqual(true)
     })
   })
   
@@ -153,17 +89,17 @@ describe('gameReducer', () => {
             {name:"Ace of Clubs", value: 1},
             {name:"Ace of Hearts", value: 1}
           ],
-          deck: this.initialState.game.deck.slice(4)
+          deck: this.initialState.deck.slice(4)
         }
       }
     })
     
     it('should deal two cards to the user, two to the ai', function() {
-      const newState = gameReducer(this.initialState.game, this.startGame)
+      const newState = gameReducer(this.initialState, this.startGame)
       expect(newState.userCards.length).toEqual(2)
     })
     it('should return a deck with 48 cards and no winner', function() {
-      const newState = gameReducer(this.initialState.game, this.startGame)
+      const newState = gameReducer(this.initialState, this.startGame)
       expect(newState.deck.length).toEqual(48)
       expect(newState.winner).toEqual(null)
     })
@@ -176,19 +112,19 @@ describe('gameReducer', () => {
         type: "HIT_USER",
         payload: {
           userCards: [ {name:"Ace of Diamonds", value: 1} ],
-          deck: this.initialState.game.deck.slice(1)
+          deck: this.initialState.deck.slice(1)
         }  
       }
     })
     
     it('should remove one card from the deck and add it to the userCards', function() {
-      const newState = gameReducer(this.initialState.game, this.hitUser)
+      const newState = gameReducer(this.initialState, this.hitUser)
       expect(newState.deck.length).toEqual(51)
       expect(newState.userCards.length).toEqual(1)
     })
     
     it('should not modify the aiCards or the winner', function() {
-      const newState = gameReducer(this.initialState.game, this.hitUser)
+      const newState = gameReducer(this.initialState, this.hitUser)
       expect(newState.aiCards).toEqual([])
       expect(newState.winner).toEqual(null)
     })
@@ -201,19 +137,19 @@ describe('gameReducer', () => {
         type: "HIT_AI",
         payload:{
           aiCards: [ {name:"Ace of Diamonds", value: 1} ],
-          deck: this.initialState.game.deck.slice(1)
+          deck: this.initialState.deck.slice(1)
         }
       }
     })
     
     it('should remove one card from the deck and add it to the aiCards', function() {
-      const newState = gameReducer(this.initialState.game, this.hitAi)
+      const newState = gameReducer(this.initialState, this.hitAi)
       expect(newState.deck.length).toEqual(51)
       expect(newState.aiCards.length).toEqual(1)
     })
     
     it('should not modify the  userCards or the winner', function() {
-      const newState = gameReducer(this.initialState.game, this.hitAi)
+      const newState = gameReducer(this.initialState, this.hitAi)
       expect(newState.userCards).toEqual([])
       expect(newState.winner).toEqual(null)
     })
@@ -222,7 +158,7 @@ describe('gameReducer', () => {
   describe('"SET_WINNER" case', function() {
     
     it('can set the winner as AI', function() {
-      const newState = gameReducer(this.initialState.game, {
+      const newState = gameReducer(this.initialState, {
         type: "SET_WINNER",
         payload: {winner: "AI"}
       })
@@ -230,7 +166,7 @@ describe('gameReducer', () => {
     })
     
     it('can set the winner as USER', function() {
-      const newState = gameReducer(this.initialState.game, {
+      const newState = gameReducer(this.initialState, {
         type: "SET_WINNER",
         payload: {winner: "USER"}
       })
@@ -238,7 +174,7 @@ describe('gameReducer', () => {
     })
     
     it('does not modify deck, userCards, or aiCards', function() {
-      const newState = gameReducer(this.initialState.game, {
+      const newState = gameReducer(this.initialState, {
         type: "SET_WINNER",
         payload: {winner: "AI"}
       })
@@ -254,7 +190,7 @@ describe('gameReducer', () => {
       expect(newState.userCards).toEqual([])
       expect(newState.aiCards).toEqual([])
       expect(newState.deck.length).toEqual(52)
-      expect(newState.deck).toEqual(this.initialState.game.deck)
+      expect(newState.deck).toEqual(this.initialState.deck)
       expect(newState.winner).toEqual(null)
     })
   })
