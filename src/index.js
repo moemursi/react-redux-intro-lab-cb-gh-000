47216885e3ac126b4b2d623a013f7abc39b37e98
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-01-06 15:43:59 -0500

    Connect app and import needed tools

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import configureStore from './store/configureStore';
 
+import { resetGame, startGame } from './actions/gameActions';
 
+const store = configureStore();
 
+store.dispatch(resetGame());
+// store.dispatch(startGame(store.getState().game.deck))
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('container')
 );
 
