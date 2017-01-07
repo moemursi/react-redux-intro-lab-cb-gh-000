bd7dd214cc6cc5cb5b021f252199ffb883639cbd
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-01-07 18:47:00 -0500

    Add robust tests for hitAi action

--- test/game-actions-test.js
+++ test/game-actions-test.js
@@ -1,62 +1,12 @@
+import expect, { createSpy, spyOn, isSpy } from 'expect'
+
 import * as gameActions from '../src/actions/gameActions'
+import { initialDeck } from '../src/lib/initialState'
 
 describe('game actions', () => {
   
   beforeEach(function(){
-    this.deck = [
-     {name:"Ace of Diamonds", value: 1},
-     {name:"Ace of Spades", value: 1},
-     {name:"Ace of Clubs", value: 1},
-     {name:"Ace of Hearts", value: 1},
-     {name:"Two of Diamonds", value: 2},
-     {name:"Two of Spades", value: 2},
-     {name:"Two of Clubs", value: 2},
-     {name:"Two of Hearts", value: 2},
-     {name:"Three of Diamonds", value: 3},
-     {name:"Three of Spades", value: 3},
-     {name:"Three of Clubs", value: 3},
-     {name:"Three of Hearts", value: 3},
-     {name:"Four of Diamonds", value: 4},
-     {name:"Four of Spades", value: 4},
-     {name:"Four of Clubs", value: 4},
-     {name:"Four of Hearts", value: 4},
-     {name:"Five of Diamonds", value: 5},
-     {name:"Five of Spades", value: 5},
-     {name:"Five of Clubs", value: 5},
-     {name:"Five of Hearts", value: 5},
-     {name:"Six of Diamonds", value: 6},
-     {name:"Six of Spades", value: 6},
-     {name:"Six of Clubs", value: 6},
-     {name:"Six of Hearts", value: 6},
-     {name:"Seven of Diamonds", value: 7},
-     {name:"Seven of Spades", value: 7},
-     {name:"Seven of Clubs", value: 7},
-     {name:"Seven of Hearts", value: 7},
-     {name:"Eight of Diamonds", value: 8},
-     {name:"Eight of Spades", value: 8},
-     {name:"Eight of Clubs", value: 8},
-     {name:"Eight of Hearts", value: 8},
-     {name:"Nine of Diamonds", value: 9},
-     {name:"Nine of Spades", value: 9},
-     {name:"Nine of Clubs", value: 9},
-     {name:"Nine of Hearts", value: 9},
-     {name:"Ten of Diamonds", value: 10},
-     {name:"Ten of Spades", value: 10},
-     {name:"Ten of Clubs", value: 10},
-     {name:"Ten of Hearts", value: 10},
-     {name:"Jack of Diamonds", value: 10},
-     {name:"Jack of Spades", value: 10},
-     {name:"Jack of Clubs", value: 10},
-     {name:"Jack of Hearts", value: 10},
-     {name:"Queen of Diamonds", value: 10},
-     {name:"Queen of Spades", value: 10},
-     {name:"Queen of Clubs", value: 10},
-     {name:"Queen of Hearts", value: 10},
-     {name:"King of Diamonds", value: 10},
-     {name:"King of Spades", value: 10},
-     {name:"King of Clubs", value: 10},
-     {name:"King of Hearts", value: 10}
-    ]
+    this.deck = initialDeck
   })
   
   describe('`resetGame()`', function(){
@@ -74,33 +24,68 @@ describe('game actions', () => {
       expect(gameActions.startGame(this.deck).type).toEqual("START_GAME")
     })
 
-    it('it returns a `payload` with the deck minus the starting hand for the user and AI', function(){
+    it('it returns an object with the `deck` minus the starting hand for the user and AI', function(){
       expect(gameActions.startGame).toBeA('function', 'startGame is not a function')
-      const startGamePayload = gameActions.startGame(this.deck).payload
-      expect(startGamePayload.deck.length).toEqual(48);
+      const startGameAction = gameActions.startGame(this.deck)
+      expect(startGameAction.deck.length).toEqual(48);
     })
 
-    it('it returns a `payload` with the user and ai cards', function(){
+    it('it returns an object with the `userCards` and `aiCards`', function(){
       expect(gameActions.startGame).toBeA('function', 'startGame is not a function')
-      const startGamePayload = gameActions.startGame(this.deck).payload
-      expect(startGamePayload.userCards.length).toEqual(2);
-      expect(startGamePayload.aiCards.length).toEqual(2);
+      const startGameAction = gameActions.startGame(this.deck)
+      expect(startGameAction.userCards.length).toEqual(2);
+      expect(startGameAction.aiCards.length).toEqual(2);
     })
   })
   
   describe('hitAI()', function(){
     it('returns with `type` "HIT_AI"', function(){
-      expect(gameActions.hitAI(this.deck, []).type).toEqual("HIT_AI")
-    })
-    it('returns with a `payload` with one less in the `deck`', function(){
-      console.log(`The deck length is ${this.deck.length}`)
-      const hitAiPayload = gameActions.hitAI(this.deck, []).payload;
-      expect(hitAiPayload.deck.length).toEqual(this.deck.length - 1);
-    })
-    it('returns with a `payload` with one more in the `aiCards`', function(){
       const aiCards = [];
-      const hitAiPayload = gameActions.hitAI(this.deck, aiCards).payload;
-      expect(hitAiPayload.aiCards.length).toEqual(aiCards.length + 1)
+      const userScore = 1;
+      expect(gameActions.hitAI(this.deck, aiCards, userScore).type).toEqual("HIT_AI")
+    })
+    it('returns with an object with a smaller `deck`', function(){
+      const aiCards = [];
+      const userScore = 1;
+      const hitAiAction = gameActions.hitAI(this.deck, aiCards, userScore);
+      expect(hitAiAction.deck.length).toBeLessThan(this.deck.length);
+    })
+    it('returns with an object with a larger `aiCards`', function(){
+      const aiCards = [];
+      const userScore = 1;
+      const hitAiAction = gameActions.hitAI(this.deck, aiCards, userScore);
+      expect(hitAiAction.aiCards.length).toEqual(aiCards.length + 1)
+    })
+    it('keeps drawing cards until the AI score is greater than the Users', function(){
+      const aiCards = [];
+      const userScore = 21;
+      const hitAiAction = gameActions.hitAI(this.deck, aiCards, userScore);
+      expect(hitAiAction.aiCards.length).toBeGreaterThan(2)
+    })
+    it('stops drawing cards as soon as the AI has a higher score than the user', function(){
+      const aiCards = [];
+      const userScore = 0;
+      const hitAiAction = gameActions.hitAI(this.deck, aiCards, userScore);
+      expect(hitAiAction.aiCards.length).toEqual(1)
+    })
+    it('returns with an object with a larger an `aiCards` value greater than the userScore', function(){
+      const aiCards = [];
+      const userScore = 20;
+      const hitAiAction = gameActions.hitAI(this.deck, aiCards, userScore);
+      const aiScore = hitAiAction.aiCards.reduce((sum, c)=> sum + c.value, 0)
+      expect(aiScore).toBeGreaterThan(userScore)
+    })
+    it('returns an object with a `winner` of of "AI" if the AI has a higher score than the user and less than 21 points', function(){
+      const aiCards = [];
+      const userScore = 1;
+      const hitAiAction = gameActions.hitAI(this.deck, aiCards, userScore);
+      expect(hitAiAction.winner).toEqual("AI")
+    })
+    it('returns an object with a `winner` of of "User" if the AI has a score greater than 21', function(){
+      const aiCards = [];
+      const userScore = 21;
+      const hitAiAction = gameActions.hitAI(this.deck, aiCards, userScore);
+      expect(hitAiAction.winner).toEqual("User")
     })
   })
   
